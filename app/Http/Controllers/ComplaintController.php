62a92327d4b7de715da720f3ed46af97236c1dd9
vendera <vendera.hadi@gmail.com>
2016-09-19 22:46:49 +0700

    contractstatus & complaint

---
+++ app/Http/Controllers/ComplaintController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use Auth;
+// load model
+use App\Models\MsComplaint;
+use App\Models\User;
+
+class ComplaintController extends Controller
+{
+    public function index(){
+		return view('complaint');
+    }
+
+    public function get(Request $request){
+    	// params
+    	$page = $request->page;
+    	$perPage = $request->rows; 
+    	$page-=1;
+    	$offset = $page * $perPage;
+        // @ -> isset(var) ? var : null
+        $sort = @$request->sort;
+        $order = @$request->order;
+        $filters = @$request->filterRules;
+        if(!empty($filters)) $filters = json_decode($filters);
+
+    	// olah data
+    	$count = MsComplaint::count();
+    	$fetch = MsComplaint::query();
+        if(!empty($filters) && count($filters) > 0){
+            foreach($filters as $filter){
+                $op = "like";
+                // tentuin operator
+                switch ($filter->op) {
+                    case 'contains':
+                        $op = 'like';
+                        break;
+                    case 'less':
+                        $op = '<=';
+                        break;
+                    case 'greater':
+                        $op = '>=';
+                        break;
+                    default:
+                        break;
+                }
+                // special condition
+                if($filter->field == 'compl_isactive'){
+                    if(strtolower($filter->value) == "yes") $filter->value = "true";
+                    else $filter->value = "false";
+                }
+                // end special condition
+                if($op == 'like') $fetch = $fetch->where(\DB::raw('lower(trim("'.$filter->field.'"::varchar))'),$op,'%'.$filter->value.'%');
+                else $fetch = $fetch->where($filter->field, $op, $filter->value);
+            }
+        }
+        $count = $fetch->count();
+        if(!empty($sort)) $fetch = $fetch->orderBy($sort,$order);
+        $fetch = $fetch->skip($offset)->take($perPage)->get();
+    	$result = ['total' => $count, 'rows' => []];
+    	foreach ($fetch as $key => $value) {
+    		$temp = [];
+    		$temp['id'] = $value->id;
+    		$temp['compl_code'] = $value->compl_code;
+    		$temp['compl_name'] = $value->compl_name;
+    		$temp['compl_isactive'] = !empty($value->compl_isactive) ? 'yes' : 'no';
+    		try{
+    			$temp['created_by'] = User::findOrFail($value->created_by)->name;
+    		}catch(\Exception $e){
+    			$temp['created_by'] = '-';
+    		}
+    		$result['rows'][] = $temp;
+    	}
+        return response()->json($result);
+    }
+
+    public function insert(Request $request){
+		$input = $request->all();
+		$input['created_by'] = Auth::id();
+		$input['updated_by'] = Auth::id();
+		return MsComplaint::create($input);    	
+    }
+
+    public function update(Request $request){
+    	$id = $request->id;
+    	$input = $request->all();
+    	$input['updated_by'] = Auth::id();
+    	MsComplaint::find($id)->update($input);
+    	return MsComplaint::find($id);
+    }
+
+    public function delete(Request $request){
+    	$id = $request->id;
+    	MsComplaint::destroy($id);
+    	return response()->json(['success'=>true]);
+    }
+}
