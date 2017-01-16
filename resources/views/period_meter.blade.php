00177c99a91b52ad19f18dfbea681d2da6a96fc9
Rachmat Setiawan <rahmat.setiawan90@gmail.com>
2017-01-16 19:54:19 +0700

    auto hitung meter cost dan meter used pas upload + success message saat selesai upload

--- resources/views/period_meter.blade.php
+++ resources/views/period_meter.blade.php
@@ -28,6 +28,13 @@ Period Meter
     <div class="container spark-screen">
         <div class="row">
             <div class="col-md-11">
+            @if(Session::has('msg'))
+                <div class="alert alert-success alert-dismissible">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                    <h4><i class="icon fa fa-check"></i> Alert!</h4>
+                    Success Upload Excel
+                </div>
+            @endif
                 <!-- content -->
                 <!-- Tabs -->
                 <div class="nav-tabs-custom">
