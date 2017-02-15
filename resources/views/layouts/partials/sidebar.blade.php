06203a9fec49f93e8c2311e4a900cc7eadbc5b74
vendera <vendera.hadi@gmail.com>
2017-02-15 22:24:21 +0700

    sidebar update

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -31,28 +31,32 @@
 
         <!-- Sidebar Menu -->
         <ul class="sidebar-menu">
-
-            <li class="treeview">
+            <?php
+              $tenancyUrls = [route('contract.index'), route('contract.confirmation'), route('contract.addendum'), route('contract.renewal'), route('contract.termination'), route('contract.unclosed')];
+              $unitUrls = [url('unit'), url('unittype'), route('floor.index')];
+            ?>
+            <li class="treeview @if(in_array(Request::url(),$tenancyUrls) || in_array(Request::url(),$unitUrls) || Request::url() == url('cost_item') || Request::url() == url('tenant') || Request::url() == url('typetenant')
+            || Request::url() == route('marketing.index') || Request::url() == url('vaccount')){{'active'}}@endif">
               <a href="#">
                 <i class="fa fa-building-o"></i> <span>TENANCY</span>
                 <span class="pull-right-container">
                   <i class="fa fa-angle-left pull-right"></i>
                 </span>
               </a>
-              <ul class="treeview-menu">
+              <ul class="treeview-menu @if(in_array(Request::url(),$tenancyUrls)){{'active'}}@endif">
                 <li>
                   <a href="#"><i class="fa fa-circle"></i> Contract
                     <span class="pull-right-container">
                       <i class="fa fa-angle-left pull-right"></i>
                     </span>
                   </a>
-                  <ul class="treeview-menu">
-                    <li><a href="{{route('contract.index')}}"><i class="fa fa-circle-o"></i> Contract</a></li>
-                    <li><a href="{{route('contract.confirmation')}}"><i class="fa fa-circle-o"></i> Contract Confirmation</a></li>
-                    <li><a href="{{route('contract.addendum')}}"><i class="fa fa-circle-o"></i> Contract Addendum</a></li>
-                    <li><a href="{{route('contract.renewal')}}"><i class="fa fa-circle-o"></i> Contract Renewal</a></li>
-                    <li><a href="{{route('contract.termination')}}"><i class="fa fa-circle-o"></i> Contract Termination</a></li>
-                    <li><a href="{{route('contract.unclosed')}}"><i class="fa fa-circle-o"></i> Unclosed Contract <span class="pull-right-container">
+                  <ul class="treeview-menu @if(in_array(Request::url(),$tenancyUrls)){{'active menu-open'}}@endif" @if(in_array(Request::url(),$tenancyUrls)) style="display:block" @endif>
+                    <li @if(Request::url() == route('contract.index')) class="active" @endif><a href="{{route('contract.index')}}"><i class="fa fa-circle-o"></i> Contract</a></li>
+                    <li @if(Request::url() == route('contract.confirmation')) class="active" @endif><a href="{{route('contract.confirmation')}}"><i class="fa fa-circle-o"></i> Contract Confirmation</a></li>
+                    <li @if(Request::url() == route('contract.addendum')) class="active" @endif><a href="{{route('contract.addendum')}}"><i class="fa fa-circle-o"></i> Contract Addendum</a></li>
+                    <li @if(Request::url() == route('contract.renewal')) class="active" @endif><a href="{{route('contract.renewal')}}"><i class="fa fa-circle-o"></i> Contract Renewal</a></li>
+                    <li @if(Request::url() == route('contract.termination')) class="active" @endif><a href="{{route('contract.termination')}}"><i class="fa fa-circle-o"></i> Contract Termination</a></li>
+                    <li @if(Request::url() == route('contract.unclosed')) class="active" @endif><a href="{{route('contract.unclosed')}}"><i class="fa fa-circle-o"></i> Unclosed Contract <span class="pull-right-container">
               <span class="label label-primary pull-right">{{$notif_unclosed}}</span>
             </span></a></li>
                   </ul>
@@ -63,34 +67,36 @@
                         <i class="fa fa-angle-left pull-right"></i>
                       </span>
                     </a>
-                    <ul class="treeview-menu">
-                      <li><a href="{{ url('unit') }}"><i class="fa fa-circle-o"></i> Unit</a></li>
-                      <li><a href="{{ url('unittype') }}"><i class="fa fa-circle-o"></i> Unit Type</a></li>
-                      <li><a href="{{route('floor.index')}}"><i class="fa fa-circle-o"></i> Floor</a></li>
+                    <ul class="treeview-menu @if(in_array(Request::url(),$unitUrls)){{'active menu-open'}}@endif" @if(in_array(Request::url(),$unitUrls)) style="display:block" @endif>
+                      <li @if(Request::url() == url('unit')) class="active" @endif><a href="{{ url('unit') }}"><i class="fa fa-circle-o"></i> Unit</a></li>
+                      <li @if(Request::url() == url('unittype')) class="active" @endif><a href="{{ url('unittype') }}"><i class="fa fa-circle-o"></i> Unit Type</a></li>
+                      <li @if(Request::url() == route('floor.index')) class="active" @endif><a href="{{route('floor.index')}}"><i class="fa fa-circle-o"></i> Floor</a></li>
 
                     </ul>
                 </li>
-                <li><a href="{{url('cost_item')}}"><i class="fa fa-circle-o"></i> Cost Items</a></li>
-                <li><a href="{{ url('tenant') }}"><i class="fa fa-circle-o"></i> Tenant/Owner</a></li>
-                <li><a href="{{url('typetenant')}}"><i class="fa fa-circle-o"></i> Tenant/Owner Type</a></li>
-                <li><a href="{{route('marketing.index')}}"><i class="fa fa-circle-o"></i> Marketing/Agent</a></li>
-                <li><a href="{{ url('vaccount') }}"><i class="fa fa-circle-o"></i> Virtual Account</a></li>
+                <li @if(Request::url() == url('cost_item')) class="active" @endif><a href="{{url('cost_item')}}"><i class="fa fa-circle-o"></i> Cost Items</a></li>
+                <li @if(Request::url() == url('tenant')) class="active" @endif><a href="{{ url('tenant') }}"><i class="fa fa-circle-o"></i> Tenant/Owner</a></li>
+                <li @if(Request::url() == url('typetenant')) class="active" @endif><a href="{{url('typetenant')}}"><i class="fa fa-circle-o"></i> Tenant/Owner Type</a></li>
+                <li @if(Request::url() == route('marketing.index')) class="active" @endif><a href="{{route('marketing.index')}}"><i class="fa fa-circle-o"></i> Marketing/Agent</a></li>
+                <li @if(Request::url() == url('vaccount')) class="active" @endif><a href="{{ url('vaccount') }}"><i class="fa fa-circle-o"></i> Virtual Account</a></li>
               
               </ul>
             </li>
 
-
-            <li class="treeview">
+            <?php
+              $engineeringUrls = [url('period_meter'), url('complaint'), route('report.hmeter')];
+            ?>
+            <li class="treeview @if(in_array(Request::url(),$engineeringUrls)){{'active'}}@endif">
               <a href="#">
                 <i class="fa fa-fire-extinguisher"></i> <span>ENGINEERING</span>
                 <span class="pull-right-container">
                   <i class="fa fa-angle-left pull-right"></i>
                 </span>
               </a>
-              <ul class="treeview-menu">
-                <li><a href="{{url('period_meter')}}"><i class="fa fa-circle-o"></i> Meter Input</a></li>
-                <li><a href="{{ url('complaint') }}"><i class="fa fa-circle-o"></i> Complain Entry</a></li>
-                <li><a href="{{route('report.hmeter')}}"><i class="fa fa-circle-o"></i> History Reports</a></li>
+              <ul class="treeview-menu ">
+                <li @if(Request::url() == url('period_meter')) class="active" @endif><a href="{{url('period_meter')}}"><i class="fa fa-circle-o"></i> Meter Input</a></li>
+                <li @if(Request::url() == url('complaint')) class="active" @endif><a href="{{ url('complaint') }}"><i class="fa fa-circle-o"></i> Complain Entry</a></li>
+                <li @if(Request::url() == route('report.hmeter')) class="active" @endif><a href="{{route('report.hmeter')}}"><i class="fa fa-circle-o"></i> History Reports</a></li>
               </ul>
             </li>
 
@@ -119,11 +125,10 @@
               </ul>
             </li> -->
 
-            
-
-            
-
-            <li class="treeview">
+            <?php
+              $arUrls = [route('invoice.generate'), route('invoice.index'), route('aging.index'), route('report.arview')];
+            ?>
+            <li class="treeview @if(in_array(Request::url(),$arUrls)){{'active'}}@endif">
               <a href="#">
                 <i class="fa fa-book"></i> <span>ACCOUNT RECEIVABLES</span>
                 <span class="pull-right-container">
@@ -131,10 +136,10 @@
                 </span>
               </a>
               <ul class="treeview-menu">
-                <li><a href="{{route('invoice.generate')}}"><i class="fa fa-circle-o"></i> Generate Invoice</a></li>
-                <li><a href="{{route('invoice.index')}}"><i class="fa fa-circle-o"></i> Invoices</a></li>
-                <li><a href="{{route('aging.index')}}"><i class="fa fa-circle-o"></i> Aging Invoices</a></li>
-                <li><a href="{{route('report.arview')}}"><i class="fa fa-circle-o"></i> AR Reports</a></li>
+                <li @if(Request::url() == route('invoice.generate')) class="active" @endif><a href="{{route('invoice.generate')}}"><i class="fa fa-circle-o"></i> Generate Invoice</a></li>
+                <li @if(Request::url() == route('invoice.index')) class="active" @endif><a href="{{route('invoice.index')}}"><i class="fa fa-circle-o"></i> Invoices</a></li>
+                <li @if(Request::url() == route('aging.index')) class="active" @endif><a href="{{route('aging.index')}}"><i class="fa fa-circle-o"></i> Aging Invoices</a></li>
+                <li @if(Request::url() == route('report.arview')) class="active" @endif><a href="{{route('report.arview')}}"><i class="fa fa-circle-o"></i> AR Reports</a></li>
                 <!-- <li><a href="#"><i class="fa fa-circle-o"></i> Outstanding</a></li> -->
                 <!-- <li>
                   <a href="#"><i class="fa fa-circle"></i> Form
@@ -244,7 +249,10 @@
               </ul> -->
             </li>
 
-            <li class="treeview">
+            <?php
+              $masterUrls = [url('coa'), url('department'), url('invtype'), url('groupaccount')];
+            ?>
+            <li class="treeview @if(in_array(Request::url(),$masterUrls)){{'active'}}@endif">
               <a href="#">
                 <i class="fa fa-gears"></i> <span>MASTER DATA</span>
                 <span class="pull-right-container">
@@ -252,19 +260,22 @@
                 </span>
               </a>
               <ul class="treeview-menu">
-                <li><a href="{{url('coa')}}"><i class="fa fa-circle-o"></i> Chart of Account</a></li>
+                <li @if(Request::url() == url('coa')) class="active" @endif><a href="{{url('coa')}}"><i class="fa fa-circle-o"></i> Chart of Account</a></li>
                 <!-- <li><a href="{{url('currency')}}"><i class="fa fa-circle-o"></i> Currency</a></li> -->
                 <!-- <li><a href="{{url('contractstatus')}}"><i class="fa fa-circle-o"></i> Contract Status</a></li> -->
-                <li><a href="{{url('department')}}"><i class="fa fa-circle-o"></i> Department</a></li>
-                <li><a href="{{url('invtype')}}"><i class="fa fa-circle-o"></i> Invoice Type</a></li>
-                <li><a href="{{url('groupaccount')}}"><i class="fa fa-circle-o"></i> Group Account</a></li>
+                <li @if(Request::url() == url('department')) class="active" @endif><a href="{{url('department')}}"><i class="fa fa-circle-o"></i> Department</a></li>
+                <li @if(Request::url() == url('invtype')) class="active" @endif><a href="{{url('invtype')}}"><i class="fa fa-circle-o"></i> Invoice Type</a></li>
+                <li @if(Request::url() == url('groupaccount')) class="active" @endif><a href="{{url('groupaccount')}}"><i class="fa fa-circle-o"></i> Group Account</a></li>
                 <!-- <li><a href="{{url('groupaccdetail')}}"><i class="fa fa-circle-o"></i> Group Account Detail</a></li> -->
                 <!-- <li><a href="{{url('rentalperiod')}}"><i class="fa fa-circle-o"></i> Rental Period</a></li> -->
                 <!-- <li><a href="{{url('supplier')}}"><i class="fa fa-circle-o"></i> Supplier</a></li> -->
               </ul>
             </li>
 
-            <li class="treeview">
+            <?php
+              $cashbankUrls = [route('payment.index'), route('cash_bank.index')];
+            ?>
+            <li class="treeview @if(in_array(Request::url(),$cashbankUrls)){{'active'}}@endif">
               <a href="#">
                 <i class="fa fa-bank"></i> <span>CASH BANK</span>
                 <span class="pull-right-container">
@@ -272,8 +283,8 @@
                 </span>
               </a>
               <ul class="treeview-menu">
-                <li><a href="{{route('payment.index')}}"><i class="fa fa-circle-o"></i> Payment Invoice</a></li>
-                <li><a href="{{route('cash_bank.index')}}"><i class="fa fa-circle-o"></i> Cash Bank List</a></li>
+                <li @if(Request::url() == route('payment.index')) class="active" @endif><a href="{{route('payment.index')}}"><i class="fa fa-circle-o"></i> Payment Invoice</a></li>
+                <li @if(Request::url() == route('cash_bank.index')) class="active" @endif><a href="{{route('cash_bank.index')}}"><i class="fa fa-circle-o"></i> Cash Bank List</a></li>
                 <!-- <li><a href="#"><i class="fa fa-circle-o"></i> Advance Entry</a></li>
                 <li><a href="#"><i class="fa fa-circle-o"></i> Unidentified Entry</a></li>
                 <li><a href="#"><i class="fa fa-circle-o"></i> Virtual Account Entry</a></li>
@@ -327,8 +338,11 @@
               </ul>
             </li>
 
-            <li><a href="{{route('journal.index')}}"><i class="fa fa-circle-o"></i> Journal Entry</a></li>
+            <li @if(Request::url() == route('journal.index')) class="active" @endif><a href="{{route('journal.index')}}"><i class="fa fa-circle-o"></i> Journal Entry</a></li>
 
+            <?php
+              $reportUrls = [];
+            ?>
             <li class="treeview">
               <a href="#">
                 <i class="fa fa-archive"></i> <span>Report</span>
