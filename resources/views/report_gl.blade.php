035b5a8efad4ba60731fcdc135bd7a88350f5511
Rachmat Setiawan <rahmat.setiawan90@gmail.com>
2017-06-14 21:07:27 +0700

    report gl

--- resources/views/report_gl.blade.php
+++ resources/views/report_gl.blade.php
@@ -16,15 +16,7 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('plugins/jquery-easyui/themes/icon.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('plugins/jquery-easyui/themes/color.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('plugins/datepicker/datepicker3.css') }}">
-    <!-- select2 -->
     <link rel="stylesheet" type="text/css" href="{{ asset('plugins/select2/select2.min.css') }}">
-    <style type="text/css">
-    .select2-container--default .select2-selection--single{
-      border-radius: 0px;
-      height: 36px;
-      width: 235px;
-    }
-    </style>
 @endsection
 
 @section('contentheader_breadcrumbs')
@@ -35,109 +27,117 @@
 @stop
 
 @section('main-content')
-		<div class="row">
-			<div class="col-md-12">
-          		<!-- content -->
-                <div class="box" style="min-height:500px">
-                    <div class="box-body">
-                        <form action="post" id="filter">
-                        <div class ="row">
-                            <div class="col-sm-3">
-                              <select name="dept" class="form-control">
-                                <option value="">All Department</option> 
-                                @foreach($departments as $dept)
-                                <option value="{{$dept->id}}" @if(Request::input('dept') == $dept->id){{'selected="selected"'}}@endif>{{$dept->dept_name}}</option>
-                                @endforeach
-                              </select>
+<div class="row">
+    <!-- left column -->
+    <div class="col-md-12">
+      <!-- general form elements -->
+      <div class="box box-primary">
+        <div class="box-header with-border">
+          <h3 class="box-title">Filter Report</h3>
+        </div>
+        <!-- /.box-header -->
+        <!-- form start -->
+        <form role="form" action="post" id="filter">
+            <div class="box-body">
+                <div class="col-sm-6">
+                    <div class="form-group">
+                        <select name="dept" class="form-control">
+                            <option value="">All Department</option> 
+                            @foreach($departments as $dept)
+                            <option value="{{$dept->id}}" @if(Request::input('dept') == $dept->id){{'selected="selected"'}}@endif>{{$dept->dept_name}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <div class="input-group date">
+                            <div class="input-group-addon">
+                                <i class="fa fa-calendar"></i>
                             </div>
-
-                            <div class="col-sm-3">
-                                <select class="form-control" name="jour_type_id">
-                                  <option value="">All Journal Type</option>
-                                  @foreach($journal_types as $jourtype)
-                                  <option value="{{$jourtype->id}}" @if(Request::input('jour_type_id') == $jourtype->id){{'selected="selected"'}}@endif>{{$jourtype->jour_type_name}}</option>
-                                  @endforeach
-                                </select>
-                            </div>
-                            
-                            <div class="col-sm-3 dates">
-                                <div class="form-group">
-                                    <div class="input-group date">
-                                        <div class="input-group-addon">
-                                            <i class="fa fa-calendar"></i>
-                                        </div>
-                                        <input type="text" class="form-control datepicker" name="from" placeholder="Date From" data-date-format="yyyy-mm-dd" value="{{date('Y-m-d')}}">
-                                    </div>
-                                </div>
-                            </div>
-                            
-                             <div class="col-sm-3 dates">
-                                <div class="form-group">
-                                    <div class="input-group date">
-                                        <div class="input-group-addon">
-                                            <i class="fa fa-calendar"></i>
-                                        </div>
-                                        <input type="text" class="form-control datepicker" name="to" placeholder="Date To" data-date-format="yyyy-mm-dd" value="{{date('Y-m-d')}}">
-                                    </div>
-                                </div>
-                            </div>
-                            
-                        </div>
-
-                        <div class="row" style="margin-bottom:15px">
-                            <div class="col-sm-3">
-                                <select name="coa" class="form-control js-example-basic-single" id="selectAccount" style="width:100%">
-                                      <option value="">From COA</option>                                     
-                                      @foreach($accounts as $key => $coa)
-                                          <option value="{{$coa->coa_code}}" data-name="{{$coa->coa_name}}">{{$coa->coa_code." ".$coa->coa_name}}</option>
-                                      @endforeach
-                                </select>
-                            </div>
-
-                            <div class="col-sm-3">
-                                <select name="tocoa" class="form-control js-example-basic-single" id="selectAccount2" style="width:100%">
-                                      <option value="">To COA</option>                                     
-                                      @foreach($accounts as $key => $coa)
-                                          <option value="{{$coa->coa_code}}" data-name="{{$coa->coa_name}}">{{$coa->coa_code." ".$coa->coa_name}}</option>
-                                      @endforeach
-                                </select>
-                            </div>
-
-                            <div class="col-sm-3">
-                                <input class="form-control" type="text" name="q" placeholder="Keyword (Tenant Name / Description / Ref No)">
-                            </div>
-
-                            <div class="col-sm-3">
-                                <button class="btn btn-info">Submit</button>
-                            </div>
-                        </div> 
-
-                        
-                        </form>
-
-                        <div class ="row" style="margin-top:80px">
-                            <div class="col-lg-12 col-md-12 col-sm-12">
-                                <button id="pdf" class="btn btn-success" style="margin-bottom:15px; display:none">Pdf</button>
-                                <button id="excel" class="btn btn-info" style="margin-bottom:15px; display:none">Excel</button>
-                                <button id="print" class="btn btn-primary" style="margin-bottom:15px; display:none">Print</button>
-                                <iframe id="frame" style="width:100%; border: 1px solid #f1ebeb; height:500px"></iframe>
-                            </div>
+                            <input type="text" class="form-control datepicker" name="from" placeholder="Date From" data-date-format="yyyy-mm-dd" value="{{date('Y-m-d')}}">
                         </div>
                     </div>
+                    <div class="form-group">
+                        <select name="coa" class="form-control choose-coa" id="selectAccount" style="width:100%">
+                          <option value="">From COA</option>                                     
+                          @foreach($accounts as $key => $coa)
+                              <option value="{{$coa->coa_code}}" data-name="{{$coa->coa_name}}">{{$coa->coa_code." ".$coa->coa_name}}</option>
+                          @endforeach
+                        </select>
+                    </div>
                 </div>
+                <div class="col-sm-6">
+                    <div class="form-group">
+                        <select class="form-control" name="jour_type_id">
+                          <option value="">All Journal Type</option>
+                          @foreach($journal_types as $jourtype)
+                          <option value="{{$jourtype->id}}" @if(Request::input('jour_type_id') == $jourtype->id){{'selected="selected"'}}@endif>{{$jourtype->jour_type_name}}</option>
+                          @endforeach
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <div class="input-group date">
+                            <div class="input-group-addon">
+                                <i class="fa fa-calendar"></i>
+                            </div>
+                            <input type="text" class="form-control datepicker" name="to" placeholder="Date To" data-date-format="yyyy-mm-dd" value="{{date('Y-m-d')}}">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <select name="tocoa" class="form-control choose-coa" id="selectAccount2" style="width:100%">
+                            <option value="">To COA</option>                                     
+                              @foreach($accounts as $key => $coa)
+                            <option value="{{$coa->coa_code}}" data-name="{{$coa->coa_name}}">{{$coa->coa_code." ".$coa->coa_name}}</option>
+                              @endforeach
+                        </select>
+                    </div>
+                </div>
+                <div class="col-sm-12">
+                    <div class="form-group">
+                        <input class="form-control" type="text" name="q" placeholder="Keyword (Tenant Name / Description / Ref No)">
+                    </div>
+                </div>    
+            </div>
+          <!-- /.box-body -->
 
-        	</div>
-		</div>
-	</div>
+          <div class="box-footer">
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
+        </form>
+      </div>
+      <!-- /.box -->
+    </div>
+</div>
+
+<div class="row">
+    <!-- left column -->
+    <div class="col-md-12">
+      <!-- general form elements -->
+      <div class="box box-primary">
+        <div class="box-header with-border">
+          <h3 class="box-title">Result</h3>
+        </div>
+        <!-- /.box-header -->
+          <div class="box-body">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12">
+                    <button id="excel" class="btn btn-info" style="margin-bottom:15px; display:none">Excel</button>
+                        <button id="print" class="btn btn-primary" style="margin-bottom:15px; display:none">Print</button>
+                        <iframe id="frame" style="width:100%; border: 1px solid #f1ebeb; height:400px"></iframe>
+                </div>
+            </div>
+          </div>
+          <!-- /.box-body -->
+      </div>
+      <!-- /.box -->
+    </div>
+</div>	
 @endsection
 
 @section('footer-scripts')
 <script src="{{asset('plugins/jQueryUI/jquery-ui.min.js')}}"></script>
 <script type="text/javascript" src="{{ asset('plugins/jquery-easyui/jquery.easyui.min.js') }}"></script>
 <script type="text/javascript" src="{{ asset('js/datagrid-filter.js') }}"></script>
-<!-- datepicker -->
 <script type="text/javascript" src="{{ asset('plugins/datepicker/bootstrap-datepicker.js') }}"></script>
-<!-- select2 -->
 <script type="text/javascript" src="{{ asset('plugins/select2/select2.min.js') }}"></script>
 <script type="text/javascript">
     $('.datepicker').datepicker({
@@ -193,6 +193,11 @@
             newWindow.focus();
         }
     }
+
+    $(".choose-coa").select2({
+        placeholder: "Select an Unit",
+        allowClear: true
+    });
 
     $('#print').click(function(){
         var url = current_url+'&print=1';
